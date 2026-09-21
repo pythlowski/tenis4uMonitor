9b005c837535862c088a430f35ccc821d3ea0e14
pythlowski <ikswoltyp@gmail.com>
2026-09-21 22:33:36 +0200

    feat: separate all logs and error logs

--- src/logger.py
+++ src/logger.py
@@ -4,11 +4,13 @@ from pathlib import Path
 
 
 class Logger:
-    
+    LOGS_DIRECTORY = "logs"
+    ALL_LOGS_FILENAME = "app.log"
+    ERROR_LOGS_FILENAME = "error.log"  
+
     def __init__(
         self,
         name: str = "APP",
-        log_file: str = "logs/app.log",
         level: int = logging.INFO,
     ):
         self.logger = logging.getLogger(name)
@@ -18,30 +20,35 @@ class Logger:
         if self.logger.handlers:
             return
 
-        log_path = Path(log_file)
-        log_path.parent.mkdir(parents=True, exist_ok=True)
-
-
         formatter = logging.Formatter(
             fmt="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
             datefmt="%Y-%m-%d %H:%M:%S",
         )
 
-        # File handler — keeps the last 5 files, max 5 MB each
-        file_handler = RotatingFileHandler(
-            log_path,
-            maxBytes=5 * 1024 * 1024,
-            backupCount=5,
-            encoding="utf-8",
-        )
-        file_handler.setFormatter(formatter)
+        Path(self.LOGS_DIRECTORY).mkdir(parents=True, exist_ok=True)
+
+        all_logs_handler = self._create_handler(self.ALL_LOGS_FILENAME, formatter, logging.INFO)
+        error_logs_handler = self._create_handler(self.ERROR_LOGS_FILENAME, formatter, logging.ERROR)
 
         # Console handler
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(formatter)
 
-        self.logger.addHandler(file_handler)
+        self.logger.addHandler(all_logs_handler)
+        self.logger.addHandler(error_logs_handler)
         self.logger.addHandler(console_handler)
+
+    def _create_handler(self, file_name: str, formatter: logging.Formatter, logLevel):
+        handler = RotatingFileHandler(
+            f"{self.LOGS_DIRECTORY}/{file_name}",
+            maxBytes=5 * 1024 * 1024,
+            backupCount=5,
+            encoding="utf-8",
+        )
+        handler.setLevel(logLevel)
+        handler.setFormatter(formatter)
+
+        return handler
 
     def debug(self, message: str, *args, **kwargs):
         self.logger.debug(message, *args, **kwargs)
